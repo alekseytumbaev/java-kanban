ec36e1669611c96702847cb699f6d16f23475d77
alekseytumbaev <leha.tumbaev@gmail.com>
2022-11-17 10:57:02 +0700

    fix: change List<Task> history to Deque<Task> history in InMemoryHistoryManager

--- src/task_managers/history_managers/InMemoryHistoryManager.java
+++ src/task_managers/history_managers/InMemoryHistoryManager.java
@@ -2,23 +2,24 @@ package task_managers.history_managers;
 
 import tasks.Task;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> history = new LinkedList<>();
+    private final Deque<Task> history = new LinkedList<>();
 
     @Override
     public void add(Task task) {
         if (task == null) return;
         if (history.size() >= 10)
-            history.remove(0);
+            history.remove();
 
         history.add(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return history;
+        return (List<Task>) history;
     }
 }
