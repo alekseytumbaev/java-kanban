99514d9bfdc6a17e5f653e1b3529e14429ffd659
alekseytumbaev <leha.tumbaev@gmail.com>
2022-12-12 13:55:17 +0700

    refactor: make Node inner class of InMemoryHistoryManager

--- src/task_managers/history_managers/InMemoryHistoryManager.java
+++ src/task_managers/history_managers/InMemoryHistoryManager.java
@@ -77,3 +77,33 @@ public class InMemoryHistoryManager implements HistoryManager {
         removeNode(nodeToRemove);
     }
 }
+
+class Node {
+    private Node prev;
+    private Node next;
+    private final Task task;
+
+    public Node(Task task) {
+        this.task = task;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+}
